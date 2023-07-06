f49337222138748e71302f48451b1fd5db601200
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 12:59:20 +0300

    basic type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains an annotated function"""
+
+
+def add(a: float, b: float) -> float:
+    """This function returns the sum of two floats"""
+    return a + b
