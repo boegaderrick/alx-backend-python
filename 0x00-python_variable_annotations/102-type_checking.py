0f8c023b22f2314a703d731d0190a0d40e771e9f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 18:40:11 +0300

    annotation - duck typing

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module containes a function that returns a list"""
+from typing import Any, List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """This function takes 2 parameters and returns a list"""
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
