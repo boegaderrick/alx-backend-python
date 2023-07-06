565ce0f90298a20de0025bb3e1a41ff72b18435c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 13:07:56 +0300

    annotation - concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a concatenation function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        This function takes two strings, concatenates them and
+        returns the resulting string
+    """
+    return str1 + str2
