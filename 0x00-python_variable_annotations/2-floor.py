7a7097cf52e233737cbfbb5fcb6074c2913ec561
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 13:17:57 +0300

    annotation - floor a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a floor function"""
+import math
+
+
+def floor(n: float) -> int:
+    """This function takes a float, floors it and returns the result"""
+    return math.floor(n)
