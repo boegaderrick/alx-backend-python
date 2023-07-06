bd6e5fd9f0834a314a12775de1104e65ddc7ee4f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 15:05:02 +0300

    annotation - return sum of list of ints and floats

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module contains a function that returns a sum"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
         This function takes a list of ints and floats and returns their sum
     """
