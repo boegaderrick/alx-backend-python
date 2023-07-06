8019aa735891d05ab04ab60de0a240b52bcdb4c5
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 13:32:50 +0300

    annotation - return sum of contents of list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module contains a function that sums floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+        This function takes a list of floats and returns the sum
+        of the floats.
+    """
+    return sum(input_list)
