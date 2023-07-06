7722f18bad77c980116523849f5ac81550d1259f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 16:44:13 +0300

    annotation - more on duck typing

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,10 +2,12 @@
 """This module contains a function that returna a random value"""
 from typing import Any, Mapping, TypeVar, Union
 
-
 T = TypeVar('T')
 alias = Union[T, None]
-def safely_get_value(dct: Mapping, key: Any, default: alias = None) -> Union[Any, T]:
+
+
+def safely_get_value(
+        dct: Mapping, key: Any, default: alias = None) -> Union[Any, T]:
     """
         This function takes three parameters and determines a return value
         using the parameters passed on the parameters passed.
