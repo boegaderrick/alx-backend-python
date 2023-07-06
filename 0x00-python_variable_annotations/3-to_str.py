6bb81a3e5ed5385c81c8137fd8d7c3c46927e73c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-06 13:23:12 +0300

    annotation - convert float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function that returns a string"""
+
+
+def to_str(n: float) -> str:
+    """
+        This function takes a float, converts it to a string & returns
+        the resulting string
+    """
+    return str(n)
