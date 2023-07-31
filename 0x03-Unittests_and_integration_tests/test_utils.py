fae6d0f16e6f4fd193c81f7b2491a6b2a2603c22
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-31 14:49:01 +0300

    add assertRaise test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Tests the return value of 'access_nested_map' function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, msg):
+        """Tests if 'access_nested_map' raises an exception when necessary"""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(msg, error.exception.__str__().strip("'"))
