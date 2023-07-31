99fedd279e053c30189c89e2f2277508d85a17da
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-31 15:33:38 +0300

    add 'utils.get_json' tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """This module contains a test class"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,3 +26,19 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(msg, error.exception.__str__().strip("'"))
+
+
+class TestGetJson(unittest.TestCase):
+    """'utils.get_json' test class definition"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests')
+    def test_get_json(self, test_url, test_payload, requests_mock):
+        """This method tests the 'utils.get_json' function"""
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
+        requests_mock.get.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
+        self.assertEqual(requests_mock.get.call_count, 1)
