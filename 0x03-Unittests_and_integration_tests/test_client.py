775c422a7fa5991d2b1b8952ee0646a23c4ed908
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-31 19:25:21 +0300

    property mock testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,3 +15,11 @@ class TestGithubOrgClient(unittest.TestCase):
         get_json_mock.return_value = {'json': 'test'}
         client = GithubOrgClient(org)
         self.assertEqual(client.org, {'json': 'test'})
+
+    def test_public_repos_url(self):
+        """Tests '_public_repos_url' property"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as prop_mock:
+            prop_mock.return_value = {'repos_url': 'test value'}
+            client = GithubOrgClient('test')
+            self.assertEqual(client._public_repos_url, 'test value')
