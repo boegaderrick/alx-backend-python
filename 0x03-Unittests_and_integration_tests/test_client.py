6a265c7bdc84bef5d671376d91995e8ab842fe14
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-01 04:10:25 +0300

    python integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,9 @@
 """This module contains tests for contents of the client module"""
 import unittest
 from client import GithubOrgClient
-from parameterized import parameterized
-from unittest.mock import patch, PropertyMock
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, PropertyMock, MagicMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -48,3 +49,46 @@ class TestGithubOrgClient(unittest.TestCase):
         """Tests has_license method"""
         client = GithubOrgClient('test')
         self.assertEqual(client.has_license(repo, license), expected)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    [(
+        TEST_PAYLOAD[0][1][0]['owner'],
+        TEST_PAYLOAD[0][1],
+        TEST_PAYLOAD[0][2],
+        TEST_PAYLOAD[0][3]
+    )]
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Integration test setup"""
+        cls.client = GithubOrgClient('google')
+
+        def sideEffect(url):
+            """
+                This method handles varying expectations of
+                calls to requests.get in the client module
+            """
+            mock = MagicMock()
+            json = cls.repos_payload if url.endswith(
+                'repos') else cls.org_payload
+            mock.json.return_value = json
+            return mock
+        cls.get_patcher = patch('requests.get', side_effect=sideEffect)
+        cls.get_patcher.start()
+
+    def test_public_repos(self):
+        """This method tests GithubOrgClient.public_method"""
+        self.assertEqual(self.client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """This method tests GithubOrgClient.public_method with license arg"""
+        self.assertEqual(self.client.public_repos(
+            license='apache-2.0'), self.apache2_repos)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Integration test teardown"""
+        cls.get_patcher.stop()
